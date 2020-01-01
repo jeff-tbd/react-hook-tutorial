ab18cac8aefaf793d24e5de6477f875dbfaf9eb4
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2020-01-02 01:12:36 +0900

    useReduce까지 공부

---
+++ src/Info.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+
+const Info = () => {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  const onChangeName = e => {
+    setName(e.target.value);
+  }
+  const onChangeNickname = e => {
+    setNickname(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log('effect');
+    return () => console.log('cleanup');
+  })
+
+  return (
+    <div>
+      <input value={name} onChange={onChangeName}/>
+      <input value={nickname} onChange={onChangeNickname}/>
+      <div>
+        <div>
+            <b>이름:</b> {name}
+          </div>
+          <div>
+            <b>닉네임: </b>
+            {nickname}
+          </div>        
+      </div>
+    </div>
+  );
+}
+
+export default Info;
